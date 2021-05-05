4f0982485db588be42c8c9a3e4b52b483159da21
Mohd. Faisal Moinuddin Suleman <51179625+faisalmoinuddin99@users.noreply.github.com>
2021-05-05 16:05:06 +0530

    Topic 1: Java Source File Structure Intro

---
+++ Java Source File Structure Introduction/B.java
@@ -0,0 +1,27 @@
+class A {
+    public static void main(String[] args){
+        System.out.println("A class");
+    }
+
+}
+
+public class B {
+    
+  public static void main(String[] args){
+        System.out.println("B class");
+    }
+
+}
+
+class C {
+  public static void main(String[] args){
+        System.out.println("C class");
+        
+    }
+    
+
+}
+
+class D {
+
+}
