c1ec61dfa06c31e96179fc2721c113450651c485
Mohd. Faisal Moinuddin Suleman <51179625+faisalmoinuddin99@users.noreply.github.com>
2021-05-07 13:38:14 +0530

    Abstract Class

---
+++ 5.Abstract classes/Test.java
@@ -0,0 +1,15 @@
+abstract class Test {
+
+    public static void main(String[] args) {
+        Test t = new Test() ;
+    }
+    
+
+}
+
+/*
+Test.java:4: error: Test is abstract; cannot be instantiated
+        Test t = new Test() ;
+                 ^
+1 error
+ */
